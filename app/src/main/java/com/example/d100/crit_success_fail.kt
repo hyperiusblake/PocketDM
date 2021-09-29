b51762ed699fad1234047abd20c6f8a246acbead
Robin Blake <robindeanblake@yahoo.com>
2021-09-29 10:37:57 -0400

    Added Styling (theme colors), Created Critical Suc/Fail Menu and Nav Graph

---
+++ app/src/main/java/com/example/d100/crit_success_fail.kt
@@ -0,0 +1,11 @@
+package com.example.d100
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class crit_success_fail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crit_success_fail)
+    }
+}
